6ce68f20a2d3ffd2477c90ade1d2c1b258633b32
Feyzullayevvv <2362145@brunel.ac.uk>
2025-05-08 19:33:11 +0100

    vertec analytics

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Analytics />
     <App />
   </BrowserRouter>
 );
